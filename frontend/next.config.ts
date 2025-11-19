c993fddc5618ebf16f22923d8bebe876f12dfc8e
ars1mm <arsimajvazi420@gmail.com>
2025-11-19 19:39:52 +0100

    Add async rewrites configuration for API routing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
    experimental:{
     optimizePackageImports: ["@chakra-ui/react","framer-motion"]
-   }
+   },
+   async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
